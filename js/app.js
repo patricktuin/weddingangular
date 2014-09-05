9dc7ac75704b2322b831548a13f5c4cb547f5030
Patrick van der Tuin <patrick@MacBook-Pro-van-Patrick.local>
2014-09-05 23:21:45 +0200

    dev changes

--- js/app.js
+++ js/app.js
@@ -1,4 +1,47 @@
-var app = angular.module('wedding', []);
+var app = angular.module('wedding', ['ngRoute']);
+
+app.config(function($routeProvider) {
+    $routeProvider.
+    	when('/', {
+			templateUrl: "templates/home.html",
+			controller: "AppCtrl"
+		}).
+		when('/programma', {
+			templateUrl: "templates/programma.html",
+			controller: "AppCtrl"
+		}).
+		when('/locatie', {
+			templateUrl: "templates/locatie.html",
+			controller: "AppCtrl"
+		}).
+		when('/ceremoniemeesters', {
+			templateUrl: "templates/ceremoniemeesters.html",
+			controller: "AppCtrl"
+		}).
+		when('/gasten', {
+			templateUrl: "templates/gasten.html",
+			controller: "AppCtrl"
+		}).
+		when('/fotos', {
+			templateUrl: "templates/fotos.html",
+			controller: "AppCtrl"
+		}).
+		when('/kadotips', {
+			templateUrl: "templates/kadotips.html",
+			controller: "AppCtrl"
+		}).
+    	when('/overnachten', {
+ 			templateUrl: 'templates/overnachten.html',
+        	controller: 'AddOrderController'
+    	});
+});
+
+app.controller("AppCtrl", function($scope) {
+    $scope.model = {
+        message: "This is my app!!!"
+    }
+});
+
 
 app.controller("TabController", function() {
 	this.tab = 1;
@@ -10,4 +53,7 @@ app.controller("TabController", function() {
 	this.setTab = function(setTab) {
 		this.tab = setTab;
 	};
+
+
+
 });
